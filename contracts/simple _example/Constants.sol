4efbf45c5a4e7b9e920f65f48df10630c60531f6
zhou <zhoubk1451@163.com>
2023-04-02 22:23:20 +0800

    add

---
+++ contracts/simple _example/Constants.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.10 ;
+contract Constans {
+
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+
+    uint public constant MY_UINT = 123;
+
+}
---
+++ contracts/simple _example/Variables.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity  ^0.8.10 ;
+
+contract Variables{
+    string public text = "hello";
+    uint public num =123;
+
+    function doSomething() public{
+        uint i = 456;
+        uint timestamp = block.timestamp;
+        address sender = msg.sender;
+    }
+}
